3b8f52b058017ce974db778ba42dc900b98c4e86
Ramon Luis Correa Medero <rlcorre@emory.edu>
2023-12-24 21:01:41 -0400

    pokemon

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     zip_safe=False,
     install_requires=_read_install_requires(),
     extras_require=_fill_extras(EXTRAS),
-    python_requires=">=3.9",
+    python_requires=">=3.5",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
